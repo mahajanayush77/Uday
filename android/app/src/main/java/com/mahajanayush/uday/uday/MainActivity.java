44ba7d1667dc639b511fd524b3e0dba98913df33
mahajanayush77 <mahajanayush77@gmail.com>
2021-05-22 20:08:31 +0530

    Initial commit

---
+++ android/app/src/main/java/com/mahajanayush/uday/uday/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mahajanayush.uday.uday;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/example/uday/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
